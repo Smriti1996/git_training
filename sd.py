dead23371d70841e0615391087ea762ea9190f93
aniks23 <aniket.purohit35@gmail.com>
2019-08-08 12:30:13 +0530

    Another commit

--- sd.py
+++ sd.py
@@ -1,11 +1,5 @@
 a = 'Akhilesh'
-<<<<<<< HEAD
 b = ' is Anand Kumar'
 c = ' good'
 s = a+b+c
 print(s)
-=======
-b = ' is Bhaiya Kumar'
-s = a+b
-print(s)
->>>>>>> 0b6d4cb70b2f3f50a30289585ca5436d163f2a3d
